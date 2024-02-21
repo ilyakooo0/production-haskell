14c9909fcb1525502fc6f3f059b7b13878bf1b51
iko <mail@iko.soy>
2024-02-21 13:07:49 +0300

    Added seminar 2

---
+++ seminars/2/src/Task.hs
@@ -0,0 +1,12 @@
+module Task
+  ( calculate,
+  )
+where
+
+import Control.Applicative
+import Control.Monad.State.Lazy
+import Data.Char
+import Data.Maybe
+
+calculate :: String -> Maybe Int
+calculate = error "TODO: calculate"
